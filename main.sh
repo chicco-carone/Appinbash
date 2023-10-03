612d9a120cd07b128ac61876ab2cb7db154fbdf7
Francesco <66080458+chicco-carone@users.noreply.github.com>
2023-10-03 12:58:59 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -37,7 +37,7 @@ enable_chaotic_aur() {
         echo "Installazione mirrorlist Chaotic AUR..."
         sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
         sudo pacman-key --lsign-key 3056513887B78AEB
-        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+        sudo pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
     fi
 
     # Controlla se la mirrorlist di Chaotic AUR è presente in Pacman
